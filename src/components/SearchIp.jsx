7fbf5d35487aa7eddcecf39ca5c0668709447aaa
Emilia Burza <emila.burza@gmail.com>
2024-10-16 19:22:39 +0200

    Fix input label in search ip component

--- src/components/SearchIp.jsx
+++ src/components/SearchIp.jsx
@@ -3,7 +3,7 @@ import Icon from '../assets/icon-arrow.svg'
 export default function SearchIp() {
     return(
         <>
-            <label for="ip-search">
+            <label htmlFor="ip-search">
                 <h1>IP Address Tracker</h1>
             </label>
             <input type="search" id="ip-search" name="q" placeholder="Search for any IP address or domain" />
