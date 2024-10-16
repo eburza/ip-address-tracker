76d2dde42ee0ae553552aced1667900aff90af39
Emilia Burza <emila.burza@gmail.com>
2024-10-16 18:33:25 +0200

    Add map component with fixed position

---
+++ src/components/Map.jsx
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function Map() {
+    const position = [52.237049, 21.017532];
+
+    return (
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A fixed location in Warsaw
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+}
