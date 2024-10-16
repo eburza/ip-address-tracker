70c117d2b71c2f336a019de0b7881eef0ee11358
Emilia Burza <emila.burza@gmail.com>
2024-10-16 19:18:52 +0200

    Clean up Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 export default function Map() {
-    const position = [52.237049, 21.017532];
+    const position = [52.237049, 21.017532]
 
     return (
       <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
@@ -16,5 +16,5 @@ export default function Map() {
           </Popup>
         </Marker>
       </MapContainer>
-    );
+    )
 }
