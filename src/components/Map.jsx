70a221cf63acbe2b15c2696c75d47f0c19c098e0
Emilia Burza <emila.burza@gmail.com>
2024-10-17 19:40:50 +0200

    Update location on map when ip data provided

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,41 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import { useContext, useEffect } from 'react'
+import IpContext from '../functions/Context'
+
+function ChangeView({ center, zoom }) {
+  const map = useMap()
+  useEffect(() => {
+    if (center[0] !== 0 && center[1] !== 0) {
+      map.setView(center, zoom)
+    }
+  }, [center, zoom, map])
+  return null
+}
 
 export default function Map() {
-    const position = [52.237049, 21.017532]
+  const { ipData, isLoading, error } = useContext(IpContext)
+  const defaultPosition = [51.1657, 10.4515]
+  const defaultZoom = 4
 
-    return (
-      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+  const position = ipData ? [ipData.latitude, ipData.longitude] : defaultPosition
+  const zoom = ipData ? 13 : defaultZoom
+
+  return (
+    <MapContainer center={defaultPosition} zoom={defaultZoom} style={{ height: '400px', width: '100%' }}>
+      <ChangeView center={position} zoom={zoom} />
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {ipData && (
         <Marker position={position}>
           <Popup>
-            A fixed location in Warsaw
+            {ipData.ip}<br/>
+            {ipData.city}, {ipData.country}
           </Popup>
         </Marker>
-      </MapContainer>
-    )
+      )}
+    </MapContainer>
+  )
 }
