a53679db3c133dfad205011af6b31ce621d062ea
Emilia Burza <emila.burza@gmail.com>
2024-10-17 17:51:39 +0200

    Add error handling logic to fetch api file

--- src/functions/getIpLocation.js
+++ src/functions/getIpLocation.js
@@ -4,9 +4,17 @@ async function getIpLocation(ipAddress) {
     try {
         const location = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}=${ipAddress}`)
         const apiLocation = await location.json()
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch IP data')
+        }
+
+        return apiLocation
     }
+
     catch (error) {
         console.log(`error: ${error}`)
+        throw error
     }
 }
 
